ed0eef3578dd7352c0d5606d8356eec9e6fe12fd
shyam <shyamkrishnanpv535@gmail.com>
2019-05-09 17:59:02 +0530

    modified

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from home import views
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name="home"),
+    path('',views.login,name="login")
+    #path("<int:pk>/", views.details, name="details")
 ]
