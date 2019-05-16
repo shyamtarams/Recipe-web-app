86f3a7805bd9df0078adb7a5747ea25d1fb7ad10
shyam <shyamkrishnanpv535@gmail.com>
2019-05-16 18:05:24 +0530

    conflict fix

--- Recipe/settings.py
+++ Recipe/settings.py
@@ -37,14 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'login',
-    #'home',
-    #'accounts',
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
-    #'star_ratings',
-    #'review',
-    #'posts1.apps.PostsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -92,20 +87,20 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+#AUTH_PASSWORD_VALIDATORS = [
+ #   {
+  #      'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+   # },
+   # {
+    #    'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    #},
+    #{
+     #   'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    #},
+    #{
+     #   'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    #},
+#]
 
 
 # Internationalization
--- Recipe/urls.py
+++ Recipe/urls.py
@@ -21,16 +21,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('home/', include('home.urls')),
-    #path('login/',include('login.urls')),
 
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-   path('posts/', include('posts.urls')),
-   # path('ratings/', include('star_ratings.urls', namespace='ratings')),
-    #path('', include('review.urls'))
-    #path('', include('posts1.urls')),
+    path('posts/', include('posts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+   path('', include('posts.urls')),
 ]
 
 if settings.DEBUG:
