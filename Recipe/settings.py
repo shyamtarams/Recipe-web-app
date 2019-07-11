e4863883db904d00081a27a8217aab13739a789a
shyamtarams <49896229+shyamtarams@users.noreply.github.com>
2019-07-11 15:02:09 +0530

    Update settings.py

--- Recipe/settings.py
+++ Recipe/settings.py
@@ -171,5 +171,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shyam.krishnan141@gmail.com'
-EMAIL_HOST_PASSWORD = 'agoanbird'
+EMAIL_HOST_PASSWORD = '******'
 
